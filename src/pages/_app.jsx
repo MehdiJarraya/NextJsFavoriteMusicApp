09abb2dff16cb035dd7b61bf5401707d3cf62b4c
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2021-09-15 12:41:15 +0100

    define genreData, toggleFavorite method and use NavBar in the top lelvel of our application

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,23 @@
 import '../common/css/global.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import Navbar from '../components/Navbar';
+import { genres } from '../data/genres';
 
 const App = ({ Component, pageProps }) => {
+  const [genresData, setGenresData] = useState(
+    genres.map((item) => ({ ...item, isFavorite: false }))
+  );
+
+  const toggleFavorite = (id) => {
+    setGenresData(
+      genresData.map((item) =>
+        item.id === id ? { ...item, isFavorite: !item.isFavorite } : { ...item }
+      )
+    );
+  };
+
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       import('agnostic-axe').then(({ AxeObserver, logViolations }) => {
@@ -12,7 +27,15 @@ const App = ({ Component, pageProps }) => {
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <Navbar>
+      <Component
+        {...pageProps}
+        genresData={genresData}
+        toggleFavorite={toggleFavorite}
+      />
+    </Navbar>
+  );
 };
 
 export default App;
